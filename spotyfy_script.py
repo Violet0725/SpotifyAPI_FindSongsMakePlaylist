1e4fafe9a9e9167a00378ca251175240518627cc
Eric Zhou <107652265+Violet0725@users.noreply.github.com>
2023-09-15 05:12:10 -0400

    Add files via upload

--- spotyfy_script.py
+++ spotyfy_script.py
@@ -3,16 +3,21 @@ import os
 import base64
 from requests import post, get
 import json
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+
 
 load_dotenv()
 
-client_id = os.getenv("CLIENT_ID")
-client_sec = os.getenv("CLIENT_SECRET")
+id = os.getenv("CLIENT_ID")
+sec = os.getenv("CLIENT_SECRET")
 
-print(client_id, client_sec)
-
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=id, client_secret=sec,
+                                               redirect_uri='https://api.spotify.com/v1/me',
+                                               scope='playlist-modify-public'))
 def get_token():
-    auth_string = client_id + ":" + client_sec
+    auth_string = id + ":" + sec
     auth_bytes = auth_string.encode("utf-8")
     auth__base64 = str(base64.b64encode(auth_bytes), "utf-8")
 
@@ -51,10 +56,33 @@ def get_songs(token, artist_id):
     json_result = json.loads(result.content)["tracks"]
     return json_result
 
+def add_songs(lst, name):
+    playlist_name = name + "'s top 10 songs"
+    user_id = sp.me()['id']
+    playlist = sp.user_playlist_create(user_id, playlist_name)
+    for song_name in lst:
+        results = sp.search(q=song_name, type='track')
+        if results['tracks']['items']:
+            track_uri = results['tracks']['items'][0]['uri']
+            sp.playlist_add_items(playlist['id'], [track_uri])
+    print(f"Playlist '{playlist_name}' has been created with your selected songs.")
+
 token = get_token()
-result = search_for_artist(token, "JayChou")
+artist_name = "蔡依林"
+result = search_for_artist(token, artist_name)
 artist_id = result["id"]
 songs = get_songs(token, artist_id)
 
+song_list = []
+
 for i, song in enumerate(songs):
     print(f"{i+1}. {song['name']}")
+    song_list.append(song['name'])
+
+res = input("Would you like to make a new playlist for these songs? (Y/N) ")
+
+if res.upper() == "Y":
+    add_songs(song_list, artist_name)
+elif res.upper() == "N":
+    pass
+
